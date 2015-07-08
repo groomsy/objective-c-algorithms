f3f3ebd96e218b0847454168519bfd05b8752367
Todd Grooms <todd.grooms@gmail.com>
2015-07-08 10:13:13 -0500

    Initial Commit

---
+++ Algorithms/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Algorithms
+//
+//  Created by Todd Grooms on 7/8/15.
+//  Copyright (c) 2015 Groomsy Dev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
